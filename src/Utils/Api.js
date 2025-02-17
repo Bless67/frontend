301bfc95823758299a0beb79eba452cda1d998e1
blessed <blesseddike70@gmail.com>
2025-02-17 15:42:26 +0100

    added redirect

--- src/Utils/Api.js
+++ src/Utils/Api.js
@@ -1,44 +1,47 @@
 import axios from "axios";
 
+
 const api = axios.create({
-  baseURL: "https://web-production-186a.up.railway.app/",
+    baseURL: "https://web-production-186a.up.railway.app/"
 });
 
 api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("accessToken");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+    config => {
+        const token = localStorage.getItem("accessToken");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
     }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
 );
 
 api.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-    if (error.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      try {
-        const refreshToken = localStorage.getItem("refreshToken");
-        const response = await axios.post("https://web-production-186a.up.railway.app/api/token/refresh/", {
-          refresh: refreshToken,
-        });
-        localStorage.setItem("accessToken", response.data.access);
-        return api(originalRequest);
-      } catch (err) {
-        console.log("Refresh token expired. Please login again.");
-        localStorage.clear();
-      
-      }
+    response => response,
+    async error => {
+        const originalRequest = error.config;
+        if (error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+            try {
+                const refreshToken = localStorage.getItem("refreshToken");
+                const response = await axios.post(
+                    "https://web-production-186a.up.railway.app/api/token/refresh/",
+                    {
+                        refresh: refreshToken
+                    }
+                );
+                localStorage.setItem("accessToken", response.data.access);
+                return api(originalRequest);
+            } catch (err) {
+                console.log("Refresh token expired. Please login again.");
+                localStorage.clear();
+                window.location.href = "/login";
+            }
+        }
+        return Promise.reject(error);
     }
-    return Promise.reject(error);
-  }
 );
-
 
 export default api;
